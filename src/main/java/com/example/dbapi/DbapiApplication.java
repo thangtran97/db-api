ff1fca199ba70b15c5dca999ebe649d7c028a266
thangth <thangth@nal.vn>
2020-08-18 10:09:21 +0700

    Initial commit

---
+++ src/main/java/com/example/dbapi/DbapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dbapi/DbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
